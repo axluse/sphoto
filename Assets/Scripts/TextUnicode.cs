d4b97d3c4e679924594754b07bfcca267b9a6cff
Kazuya Kuramoto <kazuya.kuramoto@o-10459-mac.local>
2018-06-09 17:07:46 +0900

    fontawesome定義

---
+++ Assets/Scripts/TextUnicode.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Globalization;
+namespace UnityEngine.UI {
+public class TextUnicode : Text {
+private bool disableDirty = false;
+private Regex regexp = new Regex(@"\\x(?<Value>[a-zA-Z0-9]{4})");
+protected override void OnPopulateMesh(VertexHelper toFill) {
+string cache = this.text;
+disableDirty = true;
+this.text = this.Decode( this.text );
+base.OnPopulateMesh(toFill);
+this.text = cache;
+disableDirty = false;
+}
+private string Decode(string value) {
+return regexp.Replace(value, m => ((char)int.Parse(m.Groups["Value"].Value, NumberStyles.HexNumber)).ToString());
+}
+public override void SetLayoutDirty() {
+if (disableDirty) {
+return;
+}
+base.SetLayoutDirty();
+}
+public override void SetVerticesDirty() {
+if (disableDirty) {
+return;
+}
+base.SetVerticesDirty();
+}
+public override void SetMaterialDirty() {
+if (disableDirty) {
+return;
+}
+base.SetMaterialDirty();
+}
+}
+}
